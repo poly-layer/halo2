cc95033cac83684e68a6710790a89158cad275d8
MuhtasimTanmoy <tanmoy@abgpocket.com>
2025-03-03 15:37:58 +0600

    synthesize add

--- Readme.md
+++ Readme.md
@@ -1,5 +1,9 @@
 # Halo2
 
+Types of Columns in Halo2:
+- Fixed Column → Stores constant values (e.g., precomputed lookup tables).
+- Advice Column → Stores witness data (dynamically assigned values during proving).
+- Instance Column → Stores public inputs.
 
 ## Resources
 - https://halo2.zksecurity.xyz 
--- artifact/src/main.rs
+++ artifact/src/main.rs
@@ -1,18 +1,26 @@
 use std::marker::PhantomData;
+
 use halo2_proofs::{
-    circuit::{Layouter, SimpleFloorPlanner},
+    circuit::{Layouter, SimpleFloorPlanner, Value},
     dev::MockProver,
-    plonk::{self, Circuit, ConstraintSystem},
+    plonk::{Advice, Circuit, Column, ConstraintSystem, Error, Expression, Selector},
+    poly::Rotation,
 };
+
 use ff::Field;
+
+const STEPS: usize = 5;
 
 struct TestCircuit<F: Field> {
     _ph: PhantomData<F>,
+    values: Value<Vec<F>>,
 }
 
 #[derive(Clone, Debug)]
 struct TestConfig<F: Field + Clone> {
     _ph: PhantomData<F>,
+    q_enable: Selector,
+    advice: Column<Advice>,
 }
 
 impl<F: Field> Circuit<F> for TestCircuit<F> {
@@ -20,27 +28,71 @@ impl<F: Field> Circuit<F> for TestCircuit<F> {
     type FloorPlanner = SimpleFloorPlanner;
 
     fn without_witnesses(&self) -> Self {
-        TestCircuit { _ph: PhantomData }
+        TestCircuit {
+            _ph: PhantomData,
+            values: Value::unknown(),
+        }
     }
 
-    #[allow(unused_variables)]
     fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
-        TestConfig { _ph: PhantomData }
+        let q_enable = meta.complex_selector();
+        let advice = meta.advice_column();
+
+        // define a new gate:
+        // next = curr + 1 if q_enable is 1
+        meta.create_gate("step", |meta| {
+            let curr = meta.query_advice(advice, Rotation::cur());
+            let next = meta.query_advice(advice, Rotation::next());
+            let q_enable = meta.query_selector(q_enable);
+            vec![q_enable * (curr - next + Expression::Constant(F::ONE))]
+        });
+
+        TestConfig {
+            _ph: PhantomData,
+            q_enable,
+            advice,
+        }
     }
 
-    #[allow(unused_variables)]
     fn synthesize(
         &self,
-        config: Self::Config,
-        layouter: impl Layouter<F>,
-    ) -> Result<(), plonk::Error> {
+        config: Self::Config, //
+        mut layouter: impl Layouter<F>,
+    ) -> Result<(), Error> {
+        layouter.assign_region(
+            || "steps",
+            |mut region| {
+                // apply the "step" gate STEPS = 5 times
+                for i in 0..STEPS {
+                    // assign the witness value to the advice column
+                    region.assign_advice(
+                        || "assign advice",
+                        config.advice,
+                        i,
+                        || self.values.as_ref().map(|values| values[i]),
+                    )?;
+
+                    // turn on the gate
+                    config.q_enable.enable(&mut region, i)?;
+                }
+
+                // assign the final "next" value
+                region.assign_advice(
+                    || "assign advice",
+                    config.advice,
+                    STEPS,
+                    || self.values.as_ref().map(|values| values[STEPS]),
+                )?;
+
+                Ok(())
+            },
+        )?;
         Ok(())
     }
 }
-
 fn main() {
     use halo2_proofs::halo2curves::bn256::Fr;
-    let circuit = TestCircuit::<Fr> { _ph: PhantomData };
+    let circuit = TestCircuit::<Fr> { _ph: PhantomData, values: Default::default() };
     let prover = MockProver::run(8, &circuit, vec![]).unwrap();
     prover.verify().unwrap();
 }
